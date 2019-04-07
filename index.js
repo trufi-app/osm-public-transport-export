bb31ccd28b1a457f775b1e09b0407088f23f4244
Malte Dölker <mdoelker@me.com>
2019-04-07 17:27:26 +0200

    style: clean up code

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const { getWays, getRoutes } = require('./src/OSM_downloader')
-const { convertGeoJSON } = require('./src/OSM_dataTool')
+const convertGeoJSON = require('./src/OSM_dataTool')
 const fs = require('fs')
-const path = require('path');
+const path = require('path')
 
 async function osmToGeojson(bounds, outputDir) {
-    let routes = await getRoutes(bounds)
-    let ways = await getWays(bounds)
-    let data = await convertGeoJSON({ routes: routes, ways: ways }, outputDir)
+    const routes = await getRoutes(bounds)
+    const ways = await getWays(bounds)
+    const data = await convertGeoJSON({ routes, ways }, outputDir)
+
     if (outputDir) {
-        if (!fs.existsSync(path.dirname(outputDir))) throw new Error(`Output directory does not exist`);
+        if (!fs.existsSync(path.dirname(outputDir))) {
+            throw new Error(`Output directory does not exist`)
+        }
+        
         fs.writeFileSync(path.join(outputDir, "routes.geojson"), JSON.stringify(data.geojson))
         fs.writeFileSync(path.join(outputDir, "log.text"), data.log)
         fs.writeFileSync(path.join(outputDir, "stops.json"), JSON.stringify(data.stops))
     }
+
     return data
 }
 
--- src/OSM_dataTool.js
+++ src/OSM_dataTool.js
@@ -1,62 +1,84 @@
-const { routeExtractor } = require('./route_extractor')
-exports.convertGeoJSON = async function (data) {
-    let routes = data.routes
-    let ways = data.ways
-    let routes_completes = 0
-    let routes_incompletes = 0
+const routeExtractor = require('./route_extractor')
+
+module.exports = function (data) {
+    const routes = data.routes
+    const ways = data.ways
+    const stops = {}
+    const geojson_features = []
+    let routes_complete = 0
+    let routes_incomplete = 0
     let log_file = ''
     let log_file_error = ''
-    let routes_json = []
-    const stop_map = {}
     
     for (let key in routes) {
-        let current_route = routes[key]
-        await routeExtractor(current_route, ways, stop_map)
-            .then((reponse) => {
-                routes_completes++
-                log_file += `\nDone >>> ${current_route.tags.name}`
-                let route_json = {
-                    "type": "Feature",
-                    "properties": {
-                        "stroke-width": 5,
-                        name: current_route.tags.ref,
-                        route: current_route.tags.name,
-                        stroke: '#164154'
-                    },
-                    "geometry": {
-                        "type": "LineString",
-                        "coordinates": reponse.points,
-                        "nodes": reponse.nodes,
-                    }
+        const current_route = routes[key]
+
+        try {
+            const data = routeExtractor(current_route, ways)
+
+            routes_complete++
+            log_file += `\nDone >>> ${current_route.tags.name}`
+
+            geojson_features.push({
+                "type": "Feature",
+                "properties": {
+                    "stroke-width": 5,
+                    name: current_route.tags.ref,
+                    route: current_route.tags.name,
+                    stroke: '#164154'
+                },
+                "geometry": {
+                    "type": "LineString",
+                    "coordinates": data.points,
+                    "nodes": data.nodes,
                 }
-                routes_json.push(route_json)
             })
-            .catch(error => {
-                routes_incompletes++
-                log_file_error += `--->>>\n\n${current_route.tags.name}\nhttps://www.openstreetmap.org/relation/${current_route.id}\n${error}\n\n<<<---`
+
+            // Merge stop names
+            Object.keys(data.stops).forEach(stop_id => {
+                if (stops[stop_id]) {
+                    stops[stop_id] = stops[stop_id].concat(data.stops[stop_id])
+                } else {
+                    stops[stop_id] = data.stops[stop_id]
+                }
             })
+        } catch (error) {
+            routes_incomplete++
+            log_file_error += `--->>>\n\n${current_route.tags.name}\nhttps://www.openstreetmap.org/relation/${current_route.id}\n${error.message}\n\n<<<---`
+        }
     }
-    let geojson_file = {
+
+    log_file = `\nroutes downloaded : ${routes_complete}\n\n${log_file}\n\n\nroutes to fix : ${routes_incomplete}\n\n${log_file_error}`
+
+    const geojson_feature_collection = {
         "type": "FeatureCollection",
-        "features": routes_json
+        "features": geojson_features
     }
-    log_file = `\nroutes dowloaded : ${routes_completes}\n\n${log_file}\n\n\nroutes to fix : ${routes_incompletes}\n\n${log_file_error}`
-    let pretty_stops = make_stop_name_pretty(stop_map)
-    return { geojson: geojson_file, stops: pretty_stops, log: log_file }
+    const formatted_stops = format_stop(stops)
+
+    return {
+        geojson: geojson_feature_collection,
+        stops: formatted_stops,
+        log: log_file,
+    }
 }
 
-const make_stop_name_pretty = (data_map) => {
-    const tmp_stop_map = {}
-    Object.keys(data_map).forEach(key => {
-        const orig = data_map[key]
-        tmp_stop_map[key] = {
-            stop_id: orig.stop_id,
-            stop_name: orig.stop_name
-                .filter((value, index, self) => self.indexOf(value) === index)
-                .filter(value => value !== "")
-                .join(" y ") ||
-                "innominada"
+const format_stop = (stops) => {
+    const result = {}
+
+    Object.keys(stops).forEach(stop_id => {
+        const stop_names = stops[stop_id]
+        const stop_name = stop_names
+            .filter((value, index, self) => self.indexOf(value) === index)
+            .filter(value => value !== "")
+            .join(" y ") ||
+            "innominada"
+
+        result[stop_id] = {
+            stop_id,
+            stop_name
         }
     })
-    return tmp_stop_map
+
+    return result
 }
